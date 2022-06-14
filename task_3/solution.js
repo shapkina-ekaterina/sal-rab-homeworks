069bc99f4c94071518015dfd06da65d944f56111
Shapkina Ekaterina <k547538@gmail.com>
2022-06-14 21:29:42 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -22,6 +22,9 @@ function renderCartItem(item) {
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    let itemCountText = product.count + " × " + product.price + " ₽ = ";
+    let sum = product.count * product.price;
+    itemCountText = itemCountText + sum + " ₽";
 
     // Конец решения задания №3.1.
 
