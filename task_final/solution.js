e20b85c7b8a0386c9d880d7e526c1562b91c8174
Shapkina Ekaterina <k547538@gmail.com>
2022-06-19 20:29:51 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {data: {client: name + " " + phone, order: {}, goods: []}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        //data.goods.push(goods[i].title, goods[i].count);
+       // data.goods.push(goods[i]);
+        data.data.goods[i] = {title: goods[i].title, count: goods[i].count};
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+    data.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.data.order.sum = sum;
 
     let jsonData = JSON.stringify(data);
 
